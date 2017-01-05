1a9f2bc384fe827b6951576a37940d0ec6f16a52
John Gillespie <jesgillespie@gmail.com>
2017-01-05 01:51:53 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req,res){
+    res.send('Hello, world!');
+});
+
+app.listen(8080, function () {
+    console.log("Listening on port 8080")
+});
+
+//test with window -> share -> Application > open
