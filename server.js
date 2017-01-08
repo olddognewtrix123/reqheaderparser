984b4ceae3b74ca7d48fb866d56f375f99045740
John Gillespie <jesgillespie@gmail.com>
2017-01-08 21:49:21 +0000

    added a layer to make it full stack

--- server.js
+++ server.js
@@ -4,14 +4,16 @@ var express = require ('express');
 var app = express();
 var forwarded = require('forwarded-for'); // need to make sure you have $ npm install --save forwarded-for
 var parseAcceptLanguage = require('parse-accept-language');
-//var usrg = require('express-useragent');
 var http = require('http').Server(app);
 var path = require('path');
 var port = process.env.PORT || 8080;
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(req, res) {
+app.get('/', (req, res, next) => res.sendFile(path.join(__dirname, '/index.html')));
+
+app.get('/api/infomania', function(req, res) {
+//app.get('/server.js/infomania', function(req, res) {
 
     var addressInfo = forwarded(req, req.headers);  // need to make sure you have $ npm install --save forwarded-for
         // it will return something like this:
